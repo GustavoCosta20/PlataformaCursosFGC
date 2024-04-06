c8f393cfae393240d9166c2f17823d29a776078a
Gustavo Costa <gustavo.ghcosta2@gmail.com>
2024-04-05 22:47:57 -0300

    04 - Gerenciamento e paginação das categorias

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,22 @@
-import express from 'express'
-import { database } from './database'
-import { adminJs, adminJsRouter } from './adminjs'
+import express from "express";
+import { database } from "./database";
+import { adminJs, adminJsRouter } from "./adminjs";
+import { router } from "./routes";
 
-const app = express()
+const app = express();
 
-app.use(express.static('public'))
+app.use(express.static("public"));
 
-app.use(adminJs.options.rootPath, adminJsRouter)
+app.use(adminJs.options.rootPath, adminJsRouter);
 
-const PORT = process.env.port || 3000
+app.use(router);
+
+const PORT = process.env.port || 3000;
 
 app.listen(PORT, async () => {
   await database.authenticate().then(() => {
-    console.log('DB connection successfull.')
-  })
+    console.log("DB connection successfull.");
+  });
 
-  console.log(`Server started successfuly at port ${PORT}.`)
-})
+  console.log(`Server started successfuly at port ${PORT}.`);
+});
