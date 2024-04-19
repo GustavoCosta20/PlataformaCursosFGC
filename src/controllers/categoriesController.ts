af6169362b7aef01845b1e70ddcc93f37dfacdf9
Gustavo Costa <gustavo.ghcosta2@gmail.com>
2024-04-18 21:49:00 -0300

    05 - Manutenção nas rotas de categorias

--- src/controllers/categoriesController.ts
+++ src/controllers/categoriesController.ts
@@ -2,6 +2,8 @@ import { Request, Response } from "express";
 import { categoryService } from "../services/categoryService";
 import { getPaginationParams } from "../helpers/getPaginationParams";
 
+//método para obter todas as categorias do banco de dados
+//Método para obter categorias de forma parciais de acordo com o parâmetro
 export const categoriesController = {
   index: async (req: Request, res: Response) => {
     const [page, perPage] = getPaginationParams(req.query);
--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import { categoriesController } from "./controllers/categoriesController";
 
+//Contém as rotas da API
 const router = express.Router();
 
 router.get("/categories", categoriesController.index);
