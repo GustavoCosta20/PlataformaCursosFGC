caab91ad8271304ed023c7903362e72bf33b536f
Gustavo Costa <gustavo.ghcosta2@gmail.com>
2024-04-19 21:54:05 -0300

    11 - Implementando buscas por cursos (nome)

--- src/routes.ts
+++ src/routes.ts
@@ -6,10 +6,11 @@ import { coursesController } from "./controllers/coursesController";
 const router = express.Router();
 
 router.get("/categories", categoriesController.index);
-router.get('/categories/:id', categoriesController.show);
+router.get("/categories/:id", categoriesController.show);
 
-router.get('/courses/featured', coursesController.featured);
-router.get('/courses/newest', coursesController.newest);
-router.get('/courses/:id', coursesController.show);
+router.get("/courses/featured", coursesController.featured);
+router.get("/courses/newest", coursesController.newest);
+router.get("/courses/search", coursesController.search);
+router.get("/courses/:id", coursesController.show);
 
 export { router };
